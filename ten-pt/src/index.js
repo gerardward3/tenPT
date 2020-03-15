19c66638de4effc32fbc99f80b5de1cc9fd6b5de
Gerard Ward <gerardward3@gmail.com>
2020-03-15 19:20:26 +0000

    Adds material-ui components.

---
+++ ten-pt/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+ReactDOM.render(
+    <Box mx="auto" p={1}>
+        <Button>Hello</Button>
+    </Box>,
+    document.getElementById('root')
+)
